dacfab802adb89e0ce15b56bc0b05cf561e601a5
Michael <nesterukmisha27@gmail.com>
2023-07-15 12:59:51 +0300

    false-changes

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ app.listen(4200, error => {
 	if (error) {
 		return console.log(error)
 	}
-	console.log('Server is running on port', 4200)
+	console.log('Server is running on port', process.env.PORT || 3000)
 })
