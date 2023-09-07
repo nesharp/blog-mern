4c23d6add8e341cfc65a258d12724f536298c62d
nesharp <nesterukmisha27@gmail.com>
2023-09-07 16:41:36 +0300

    cors-support-added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import multer from 'multer'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import { checkAuth, handleErrors } from './utils/index.js'
 import { UserController, PostController } from './controllers/index.js'
 import {
@@ -29,6 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 //---------------------------------------routes---------------------------------------
 // register
